3f623218538de0a95b510cd715e2fa73243c6aa0
Кулешов Максим <Maxkyl3d@mail.ru>
2018-10-02 12:55:30 +0300

    Update Task6.java

--- Java1009/src/com/mKuleshov/lemon/HW1/Task6.java
+++ Java1009/src/com/mKuleshov/lemon/HW1/Task6.java
@@ -2,8 +2,27 @@ package com.mKuleshov.lemon.HW1;
 
 public class Task6 {
     //Напишите метод, который будет увеличивать заданный элемент массива на 10%.
-    public static void main(String[] args) {
-        int m [] = [10,20,30,40];
+ public static void main(String[] args) {
+
+        System.out.println(increase(new int[] {10,20,30,40,50}));
+
+    }
+    static int increase(int[] a) {
+
+
+        for (int i=0; i<a.length; i++){
+            System.out.print(i+" - ");
+            System.out.println(a[i]);
+        }
+
+        System.out.println("\nУвеличение массива на 10% \n");
+
+        for (int k=0; k<a.length;k++){
+            a[k]=(int) (a[k]*0.50);
+            System.out.print(k+" - ");
+            System.out.println(a[k]);
+        }
+        return 0 ;
 
     }
 }
